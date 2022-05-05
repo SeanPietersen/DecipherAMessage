94560938c013fbd95c510b3bb038a6597e30940d
Sean Pietersen <seanpietersen7@gmail.com>
2022-05-05 10:44:05 +0200

    Feat: Added Test

---
+++ DecipherAMessage/DecipherAMessage.App/DecipherAHiddenStringMessageMethod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecipherAMessage.App
+{
+    public class DecipherAHiddenStringMessageMethod : IDecipherAHiddenStringMessageMethod
+    {
+        public string UsingROT13ToDeciperAString(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DecipherAMessage/DecipherAMessage.Test/DecipherAHiddenStringMessageTest.cs
@@ -0,0 +1,37 @@
+using DecipherAMessage.App;
+using System;
+using Xunit;
+
+namespace DecipherAMessage.Test
+{
+    public class DecipherAHiddenStringMessageTest
+    {
+        [Fact]
+        public void UsingROT13ToDeciperAStringTest1_Successful()
+        {
+            //Arrange
+            IDecipherAHiddenStringMessageMethod decipherAHiddenStringMessage = new DecipherAHiddenStringMessageMethod();
+            var input = "EBG13 rknzcyr.";
+            var expected = "ROT13 example.";
+
+            //Act
+            var actual = decipherAHiddenStringMessage.UsingROT13ToDeciperAString(input);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void UsingROT13ToDeciperAStringTest2_Successful()
+        {
+            //Arrange
+            IDecipherAHiddenStringMessageMethod decipherAHiddenStringMessage = new DecipherAHiddenStringMessageMethod();
+            var input = "This is my first ROT13 excercise!";
+            var expected = "Guvf vf zl svefg EBG13 rkprepvfr!";
+
+            //Act
+            var actual = decipherAHiddenStringMessage.UsingROT13ToDeciperAString(input);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
